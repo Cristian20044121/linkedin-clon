ddfd35b1d344c66adb683f322b3173add15c6c46
Cristian20044121 <crdcaro01@gmail.com>
2025-01-26 01:50:27 -0500

    `Update import path for userReducer and remove unused code`

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -58,11 +58,11 @@ export const selectUser = (state) => state.user.user;
 
 // We can also write thunks by hand, which may contain both sync and async logic.
 // Here's an example of conditionally dispatching actions based on current state.
-export const incrementIfOdd = (amount) => (dispatch, getState) => {
-  const currentValue = selectCount(getState());
-  if (currentValue % 2 === 1) {
-    dispatch(incrementByAmount(amount));
-  }
-};
+// export const incrementIfOdd = (amount) => (dispatch, getState) => {
+//   const currentValue = selectCount(getState());
+//   if (currentValue % 2 === 1) {
+//     dispatch(incrementByAmount(amount));
+//   }
+// };
 
 export default userSlice.reducer;
