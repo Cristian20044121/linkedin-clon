01f006fff0f187653370de3b8055b19eb1ca8c8e
Cristian20044121 <crdcaro01@gmail.com>
2025-01-28 07:27:09 -0500

    `feat: updated App.js to use useEffect for auth state management`

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { Header } from "./Header";
 import { Sidebar } from "./Sidebar";
 import { Feed } from "./Feed";
 import { Login } from "./Login"; // Asegúrate de tener este componente creado e importado
-import { useSelector } from "react-redux";
-import { selectUser } from "./features/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
+import { auth } from "./firebase";
 
 function App() {
   const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatch(
+          login({
+            email: userAuth.email,
+            uid: userAuth.uid,
+            displayName: userAuth.displayName,
+            photoUrl: userAuth.photoURL,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, []);
 
   return (
     <div className="app">
